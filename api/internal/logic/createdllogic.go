6e467a05c5a2db36f7e262c67b7affed4d190dd5
kongfuguagua <a2550597703@163.com>
2024-11-06 13:43:10 +0800

    1

--- api/internal/logic/createdllogic.go
+++ api/internal/logic/createdllogic.go
@@ -5,6 +5,7 @@ import (
 
 	"dl/api/internal/svc"
 	"dl/api/internal/types"
+	"dl/rpc/dlfunction"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -24,7 +25,10 @@ func NewCreateDLLogic(ctx context.Context, svcCtx *svc.ServiceContext) *CreateDL
 }
 
 func (l *CreateDLLogic) CreateDL(req *types.DLCreateRequest) (resp *types.DLCreateResponse, err error) {
-	// todo: add your logic here and delete this line
+	in := &dlfunction.DLCreateRequest{
+		Spec: &req.Spec,
+	}
+	l.svcCtx.DLApp.CreateDL(l.ctx, in)
 
 	return
 }
