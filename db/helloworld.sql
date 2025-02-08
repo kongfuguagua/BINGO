9b077a9f679d46f85f562bfd7eb8862b2cb06d6d
kongfuguagua <a2550597703@163.com>
2025-02-08 20:16:16 +0800

    add api

--- db/helloworld.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `World`
-(
-  `ID` varchar(255) NOT NULL COMMENT 'id',
-  `Namespace` varchar(255) NOT NULL COMMENT 'namespace',
-  `DLName` varchar(255) NOT NULL COMMENT 'dlname',
-  PRIMARY KEY(`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
